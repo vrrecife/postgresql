c42326f7414c5d8746730991d1f4ace741e3da5c
vrrecife <96882111+vrrecife@users.noreply.github.com>
2022-01-04 12:19:50 -0300

    Update get_cores.sh

--- get_cores.sh
+++ get_cores.sh
@@ -1,3 +1,4 @@
-CPU_CORES='cat /proc/cpuinfo | grep cores | wc -l'
-MAX_PARALLEL_CORES=(($CPU_CORES/2)| bc -l | xargs printf "%.0f")
+CPU_CORES=`cat /proc/cpuinfo | grep cores | wc -l`
+CPU_CORES_2=CPU_CORES/2
+MAX_PARALLEL_CORES=$CPU_CORES_2 | bc -l | xargs printf "%.0f"
 echo $MAX_PARALLEL_CORES
