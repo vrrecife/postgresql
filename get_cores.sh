ce19c5c4a29a381e83492343b577eab687b228f9
vrrecife <96882111+vrrecife@users.noreply.github.com>
2022-01-04 15:04:27 -0300

    Update get_cores.sh

--- get_cores.sh
+++ get_cores.sh
@@ -1,4 +1,9 @@
 CPU_CORES=`cat /proc/cpuinfo | grep cores | wc -l`
-CPU_CORES_2=CPU_CORES/2
-MAX_PARALLEL_CORES=$CPU_CORES_2 | bc -l | xargs printf "%.0f"
-echo $MAX_PARALLEL_CORES
+
+if [ $CPU_CORES -eq 1 ]; then
+	sed -i "/#max_parallel_workers_per_gather/ s/2/1/" /var/lib/pgsql/12/data/postgresql.conf
+	sed -i "/#max_parallel_maintenance_workers/ s/0/1/" /var/lib/pgsql/12/data/postgresql.conf
+else
+	sed -i "/#max_parallel_workers_per_gather/ s/2/$MAX_PARALLEL_CORES/" /var/lib/pgsql/12/data/postgresql.conf
+	sed -i "/#max_parallel_maintenance_workers/ s/0/$MAX_PARALLEL_CORES/" /var/lib/pgsql/12/data/postgresql.conf
+fi
