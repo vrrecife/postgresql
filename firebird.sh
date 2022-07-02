bee61bfe1fab997e883c8ed4fb5d431c85313de7
vrrecife <96882111+vrrecife@users.noreply.github.com>
2022-07-02 13:41:57 -0300

    Add files via upload

---
+++ firebird.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Firebird
+
+sudo apt update 2> /dev/null && sudo apt upgrade 2> /dev/null && sudo apt-get install libstdc++5 libncurses5 libtommath1 2> /dev/null
+
+wget -c -P $HOME https://github.com/FirebirdSQL/firebird/releases/download/R2_5_9/FirebirdSS-2.5.9.27139-0.amd64.tar.gz
+
+cd $HOME
+
+tar -xzvf FirebirdSS-2.5.9.27139-0.amd64.tar.gz
+
+cd FirebirdSS-2.5.9.27139-0.amd64
+
+sudo ./install.sh
+
+systemctl start firebird.service
+
