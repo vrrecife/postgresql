c6f13f3b8e31557d8e70c5b0eb8e92ed92c29759
vrrecife <96882111+vrrecife@users.noreply.github.com>
2022-07-02 13:01:57 -0300

    Add files via upload

---
+++ ubuntu.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# Script para instalação do Firebird | PostgreSQL | MariaDB | Microsoft SQL Server 2019
+
+sudo apt update 2> /dev/null && sudo apt upgrade 2> /dev/null && sudo apt-get install libstdc++5 libncurses5 libtommath1 2> /dev/null
