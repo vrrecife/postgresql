d261d5564934d9904986ff69c7140b878c642521
vrrecife <96882111+vrrecife@users.noreply.github.com>
2022-07-02 14:33:12 -0300

    Add files via upload

--- mariadb.sh
+++ mariadb.sh
@@ -2,4 +2,4 @@
 
 # MariaDB
 
-sudo apt update 2> /dev/null && sudo apt install mariadb-server 2> /dev/null && sudo mysql_secure_installation 2> /dev/null
+sudo apt update 2> /dev/null && sudo apt -y install mariadb-server 2> /dev/null && sudo mysql_secure_installation 2> /dev/null
