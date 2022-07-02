d1af78937f8577218aec708cd839dcc1a5267d1c
vrrecife <96882111+vrrecife@users.noreply.github.com>
2022-07-02 14:28:56 -0300

    Add files via upload

--- postgresql.sh
+++ postgresql.sh
@@ -2,12 +2,8 @@
 
 # PostgreSQL
 
-sudo apt update update 2> /dev/null && sudo apt upgrade
+sudo apt update 2> /dev/null && sudo apt upgrade 2> /dev/null
 curl -fsSL https://www.postgresql.org/media/keys/ACCC4CF8.asc|sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/postgresql.gpg
 echo "deb http://apt.postgresql.org/pub/repos/apt/ `lsb_release -cs`-pgdg main" |sudo tee /etc/apt/sources.list.d/pgdg.list
-sudo apt update sudo apt -y install postgresql-12 postgresql-contrib
+sudo apt update 2> /dev/null && sudo apt -y install postgresql-12 postgresql-contrib 2> /dev/null
 systemctl restart postgresql.service
-sudo su - postgres
-psql -c "alter user postgres with password 'VrPost@Server'"
-systemctl restart postgresql.service
-systemctl status postgresql.service
